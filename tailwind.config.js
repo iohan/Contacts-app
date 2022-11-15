f653b68df055748e6e56c0978c0b43108034129e
iohan <johan.ostling@gmail.com>
2022-11-15 21:24:04 +0100

    Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      soot: "#343b39",
+      spruce: "#4c5553",
+      clear: "#e1eae8",
+      cold: "#f0fdfa",
+      silver: "#bfd8ce",
+      wood: "#c49d88",
+      hellion: "#88c4ad",
+      white: "#fff",
+    },
     extend: {},
   },
   plugins: [],
