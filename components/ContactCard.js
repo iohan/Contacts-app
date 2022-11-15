bd68bc2231f9d410f75deda88501fafede3b9fb9
iohan <johan.ostling@gmail.com>
2022-11-15 21:24:34 +0100

    Added custom colors

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -1,10 +1,10 @@
-const ContactCard = ({ contact, contactKey, onClick }) => {
+const ContactCard = ({ contact, onClick }) => {
   return (
-    <div className="bg-teal-200 flex items-center rounded-lg p-2 mb-2" onClick={() => onClick(contactKey)}>
-      <div className="bg-teal-600 rounded-full text-teal-50 w-10 h-10">
+    <div className="bg-clear flex items-center rounded-lg p-2 mb-2" onClick={() => onClick(contact)}>
+      <div className="bg-spruce rounded-full text-cold w-10 h-10">
         <div className="text-center text-2xl pt-1 uppercase">{contact.firstName.charAt(0)}</div>
       </div>
-      <div className="pl-2">
+      <div className="pl-2 text-soot">
         <div className="text-lg">
           {contact.firstName} {contact.lastName}
         </div>
