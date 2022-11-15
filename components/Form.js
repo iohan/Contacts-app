e1d6b5ccdaae5aa32ecd2f5fc7131797c210e126
iohan <johan.ostling@gmail.com>
2022-11-15 21:24:51 +0100

    Moved inputs to own component

--- components/Form.js
+++ components/Form.js
@@ -1,41 +1,38 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import Input from "./Input";
 
-const FormError = function ({ errorMessage }) {
-  return <p className="text-red-100 mt-1">{errorMessage}</p>;
-};
-
-const Form = function ({ onSubmit, onCancel, onDelete, selectedContact }) {
+const Form = function ({ onSubmit, onCancel, onDelete, contact }) {
   const {
     register,
     handleSubmit,
     formState,
     formState: { errors },
-    formState: { isSubmitSuccessful },
     setValue,
     reset,
+    clearErrors,
   } = useForm();
 
   useEffect(() => {
-    if (selectedContact) {
-      setValue("firstName", selectedContact.firstName);
-      setValue("lastName", selectedContact.lastName);
-      setValue("phone", selectedContact.phone);
-      setValue("email", selectedContact.email);
+    if (contact) {
+      setValue("firstName", contact.firstName);
+      setValue("lastName", contact.lastName);
+      setValue("phone", contact.phone);
+      setValue("email", contact.email);
     }
     if (formState.isSubmitSuccessful) {
-      reset({ firstName: "", lastName: "", phone: "", email: "" });
+      reset();
     }
-  }, [selectedContact, formState]);
+  }, [contact, formState]);
 
   const handleCancelButton = (e) => {
     e.preventDefault();
-    reset({ firstName: "", lastName: "", phone: "", email: "" });
+    reset();
     onCancel();
   };
 
   const handleDeleteButton = () => {
-    reset({ firstName: "", lastName: "", phone: "", email: "" });
+    reset();
     onDelete();
   };
 
@@ -43,36 +40,31 @@ const Form = function ({ onSubmit, onCancel, onDelete, selectedContact }) {
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col px-3 mb-2">
-          <label className="pb-1">First Name</label>
-          <input type="text" name="firstName" {...register("firstName", { required: true })} className="px-2 py-1" />
-          {errors.firstName && <FormError errorMessage="First name is required" />}
+          <Input name="firstName" title="First Name" errors={errors} isRequired={true} register={register} />
         </div>
         <div className="flex flex-col px-3 mb-2">
-          <label className="pb-1">Last Name</label>
-          <input type="text" name="lastName" {...register("lastName")} className="px-2 py-1" />
+          <Input name="lastName" title="Last Name" errors={errors} register={register} />
         </div>
         <div className="flex flex-col px-3 mb-2">
-          <label className="pb-1">Phone number</label>
-          <input type="text" name="phone" {...register("phone")} className="px-2 py-1" />
+          <Input name="phone" title="Phone number" errors={errors} register={register} />
         </div>
         <div className="flex flex-col px-3 mb-2">
-          <label className="pb-1">E-mail</label>
-          <input type="text" name="email" {...register("email")} className="px-2 py-1" />
+          <Input name="email" title="E-mail" errors={errors} register={register} />
         </div>
         <div className="mb-2 flex flex-row gap-x-2 justify-between px-3">
-          <button type="submit" className="bg-slate-100 py-2 flex-1">
-            {selectedContact ? "Update Contact" : "Add Contact"}
+          <button type="submit" className="bg-hellion rounded text-soot py-2 flex-1">
+            {contact ? "Update Contact" : "Add Contact"}
           </button>
-          {selectedContact && (
-            <button onClick={handleCancelButton} className="bg-red-100 flex-1">
+          {contact && (
+            <button onClick={handleCancelButton} className="bg-silver rounded text-soot flex-1">
               Cancel
             </button>
           )}
         </div>
       </form>
-      {selectedContact && (
+      {contact && (
         <div className="mt-5 mb-2 flex flex-col px-3">
-          <button className="bg-red-300 text-red-900 py-2 flex-1" onClick={handleDeleteButton}>
+          <button className="bg-wood text-soot py-2 flex-1" onClick={handleDeleteButton}>
             Delete Contact
           </button>
         </div>
